4cd8f9a90e8dc76b6dc79cadba145028f06e1683
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2023-12-17 22:00:01 +0100

    nuova mappa completa

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -96,7 +96,7 @@ def single_run(index_run, random_seed, file_name, move_consumption=1.0, move_hea
                             move_heavy_consumption)
     tp = TokenPassing(agents, dimensions, obstacles, non_task_endpoints, charging_stations, simulation,
                       goal_locations, a_star_max_iter=max_iter, new_recovery=True)
-    while tp.get_completed_tasks() != len(tasks) and simulation.get_time() < 5000:
+    while tp.get_completed_tasks() != len(tasks) and simulation.get_time() < 20000:
         simulation.time_forward(tp)
 
     completed_tasks = tp.get_completed_tasks()
@@ -133,10 +133,10 @@ if __name__ == '__main__':
     sum_cbs_calls_recharge1 = 0
     sum_dead_agents1 = 0
 
-    file_name = 'Comparisons/big_change.txt'
-    move_consumption = 0.1
+    file_name = 'Comparisons/Big/test10.txt'
+    move_consumption = 0.01
     move_heavy_consumption = move_consumption
-    wait_consumption = 0.1
+    wait_consumption = 0.01
 
     with open('Comparisons/seeds2.txt', 'r') as file:
         # inserisci ogni riga in una lista
@@ -162,21 +162,32 @@ if __name__ == '__main__':
         sum_dead_agents1 += dead_agents
 
     # print("\nVersioneChange")
+    print("move_consumption: ", move_consumption)
+    print("move_heavy_consumption: ", move_heavy_consumption)
+    print("wait_consumption: ", wait_consumption)
     print("Numero di run completate: ", run_complete1)
     print("Numero medio di task completati: ", sum_completed_tasks1 / 20)
     print("Numero medio di agenti morti: ", sum_dead_agents1 / 20)
-    print("Makespan medio: ", sum_makespan1 / run_complete1)
-    print("Tempo medio di servizio: ", sum_service_time1 / run_complete1)
-    print("Deviazione standard media: ", sum_std_dev1 / run_complete1)
-    print("Chiamate a CBS per stazioni di ricarica: ", sum_cbs_calls_recharge1 / run_complete1)
-    print("Chiamate a CBS totali: ", sum_cbs_calls1 / run_complete1)
+    try:
+        print("Makespan medio: ", sum_makespan1 / run_complete1)
+        print("Tempo medio di servizio: ", sum_service_time1 / run_complete1)
+        print("Deviazione standard media: ", sum_std_dev1 / run_complete1)
+        print("Chiamate a CBS per stazioni di ricarica: ", sum_cbs_calls_recharge1 / run_complete1)
+        print("Chiamate a CBS totali: ", sum_cbs_calls1 / run_complete1)
+    except:
+        print("0 run completate")
+
 
     with open(file_name, 'a') as file:
         file.write("\n\n" + "Numero di run completate: " + str(run_complete1) + "\n")
         file.write("Numero medio di task completati: " + str(sum_completed_tasks1 / 20) + "\n")
         file.write("Numero medio di agenti morti: " + str(sum_dead_agents1 / 20) + "\n")
-        file.write("Makespan medio: " + str(sum_makespan1 / run_complete1) + "\n")
-        file.write("Tempo medio di servizio: " + str(sum_service_time1 / run_complete1) + "\n")
-        file.write("Deviazione standard media: " + str(sum_std_dev1 / run_complete1) + "\n")
-        file.write("Chiamate a CBS per stazioni di ricarica: " + str(sum_cbs_calls_recharge1 / run_complete1) + "\n")
-        file.write("Chiamate a CBS totali: " + str(sum_cbs_calls1 / run_complete1) + "\n")
+        try:
+            file.write("Makespan medio: " + str(sum_makespan1 / run_complete1) + "\n")
+            file.write("Tempo medio di servizio: " + str(sum_service_time1 / run_complete1) + "\n")
+            file.write("Deviazione standard media: " + str(sum_std_dev1 / run_complete1) + "\n")
+            file.write("Chiamate a CBS per stazioni di ricarica: " + str(sum_cbs_calls_recharge1 / run_complete1) + "\n")
+            file.write("Chiamate a CBS totali: " + str(sum_cbs_calls1 / run_complete1) + "\n")
+        except:
+            file.write("0 run completate")
+
